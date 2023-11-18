3a4412e1928939c7a1d062bfe8fc8fd690457fc8
miniity <miniitiy.18@gmail.com>
2023-11-18 10:37:24 +0900

    修正①

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,9 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
+  before_action :check_user, only: [:edit, :update, :destroy]
+
   def index
     @prototypes = Prototype.includes(:user).all
   end
@@ -34,17 +37,11 @@ class PrototypesController < ApplicationController
     end
   end
 
-#  def create
-#    @prototype = Prototype.new(prototype_params)
-#    @prototype.save
-#    redirect_to action: 'index'
-#  end
-
   def create
     @prototype = Prototype.new(prototype_params)
     @prototype.user_id = current_user.id
     if @prototype.save
-      redirect_to prototype_path(@prototype)
+      redirect_to root_path
     else
       puts "Form Data:"
       puts prototype_params.inspect
@@ -63,5 +60,18 @@ class PrototypesController < ApplicationController
       redirect_to action: :index
     end
   end
+
+  def check_user
+    # ログインユーザーと編集対象のプロトタイプのユーザーが一致しない場合、トップページにリダイレクト
+    unless current_user == @prototype.user
+      redirect_to root_path
+    end
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+
 
 end
