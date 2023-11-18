15a2c5ae2c1019eaf7d9016ef07b883a1376a558
miniity <miniitiy.18@gmail.com>
2023-11-18 14:31:15 +0900

    修正4-④

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
-  before_action :check_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show, :new, :create, :user]
+  before_action :move_to_index, except: [:index, :show]
   before_action :set_prototype, only: [:show, :edit, :update, :destroy]
   before_action :check_user, only: [:edit, :update, :destroy]
 
